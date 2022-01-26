4395f946f0b4cad564c0c404015b5cecb6835bec
Ethan-png <51973311+Ethan-png@users.noreply.github.com>
2022-01-25 16:34:23 -0800

    page
    finished has_capacity & write

--- lstore/page.py
+++ lstore/page.py
@@ -1,4 +1,7 @@
 
+from operator import truediv
+
+
 class Page:
 
     def __init__(self):
@@ -6,12 +9,17 @@ class Page:
         self.data = bytearray(4096)
 
     def has_capacity(self):
+        if(self.num_records < len(self.data)):
+            return True
+        return False
+
 
     def write(self, value):
         if self.has_capacity():
+            self.data[self.num_records] = value
             self.num_records += 1
-            self.data.append(value)
             print("Written" + value)
         else:
+            print("Could not write")
 
 
