6faab761b3b5b7f485a44d4feb4e5b09319f6433
Ethan-png <51973311+Ethan-png@users.noreply.github.com>
2022-02-04 13:25:50 -0800

    Update index.py
    using table functions

--- lstore/index.py
+++ lstore/index.py
@@ -4,6 +4,7 @@ A data strucutre holding indices for various columns of a table. Key column shou
 
 from asyncio.windows_events import NULL
 from operator import contains
+import this
 
 
 def cmp(a, b):
@@ -65,7 +66,8 @@ class Index:
     # Returns the RIDs of all records with values in column "column" between "begin" and "end"
     """
 
-    def locate_range(self, begin, end, column):
+    def locate_range(self, begin, end, index = this.table.primary_key_column):
+        column = this.indices[index]
         startIndex = -1
         endIndex = -1
         low = 0
@@ -74,21 +76,21 @@ class Index:
 
         while (low <= high):
             mid = int((high-low)/2 + low)
-            if (lookup(column[mid]) > begin):
+            if (this.table.get_value(column[mid], index) > begin):
                 high = mid - 1;
-            elif (lookup(column[mid] == begin)):
+            elif (this.table.get_value(column[mid], index) == begin):
                 startIndex = mid;
                 high = mid - 1;
             else:
                 low = mid + 1;
         #Uses Binary search again to find endIndex
         low = 0
-        high = len(a)
+        high = len(column)
         while (low <= high):
             mid = int((high-low)/2 + low)
-            if (lookup(column[mid] > end)):
+            if (this.table.get_value(column[mid], index) > end):
                 high = mid - 1;
-            elif (lookup(column[mid] == end)):
+            elif (this.table.get_value(column[mid], index) == end):
                 endIndex = mid;
                 low = mid - 1;
             else:
