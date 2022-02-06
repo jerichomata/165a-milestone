332385b4101a2e0eec703c31f90f3fc6bc24578a
jerichomata <74013719+jerichomata@users.noreply.github.com>
2022-02-05 17:48:56 -0800

    Still have bugs for udpate

--- lstore/query.py
+++ lstore/query.py
@@ -1,5 +1,6 @@
-from lstore.table import Table, Record
+from lstore.table import SCHEMA_ENCODING_COLUMN, Table, Record
 from lstore.index import Index
+
 
 
 class Query:
@@ -12,7 +13,7 @@ class Query:
 
     def __init__(self, table):
         self.table = table
-        pass
+        
 
     """
     # internal Method
@@ -22,7 +23,14 @@ class Query:
     """
 
     def delete(self, primary_key):
-        pass
+        rid = self.table.record_does_exist(primary_key)
+        if rid == None:
+            return False
+        
+        # Once found, update delete value to true in page directory
+        self.table.page_directory[rid]["deleted"] = True
+        return True
+
     """
     # Insert a record with specified columns
     # Return True upon succesful insertion
@@ -30,8 +38,27 @@ class Query:
     """
 
     def insert(self, *columns):
-        schema_encoding = '0' * self.table.num_columns
-        pass
+        # schema_encoding = '0' * self.table.num_columns
+        unique_identifier = columns[0]
+        columns_list = list(columns)
+        if len(columns_list) != self.table.num_columns:
+            return False
+        if not self._check_values_are_valid(columns_list):
+            return False
+        # if self.table.record_does_exist(key=unique_identifier) != None:
+        #     return False
+
+        # New record passed the checks, set schema encoding to 0, create a new record, and write to the table
+        blank_schema_encoding = 0
+        new_rid = self.table.new_base_rid()
+        self.table.index_on_primary_key[unique_identifier] = new_rid
+        new_record = Record(key=unique_identifier, rid=new_rid, schema_encoding=blank_schema_encoding, column_values=columns_list)
+        did_successfully_write = self.table.write_new_record(record=new_record, rid=new_rid)
+
+        if did_successfully_write:
+            return True
+        else:
+            return False
 
     """
     # Read a record with specified key
@@ -43,7 +70,39 @@ class Query:
     """
 
     def select(self, index_key, column, query_columns):
-        pass
+        if column > self.table.num_columns or column < 0:
+            # column argument out of range
+            return False
+        if len(query_columns) != self.table.num_columns:
+            # length of query columns must equal the number of columns in the table
+            return False
+        for value in query_columns:
+            # incoming query column values must be 0 or 1
+            if value != 0 and value != 1:
+                return False
+
+        # Make sure that the record selected by the user exists in our database
+        valid_rid = self.table.record_does_exist(index_key=index_key)
+        if valid_rid == None:
+            return False
+
+        # If exists, read the most update record by looking at the scheme encoding
+        # Values with 0 will come from the base pages, values with a 1 will come from the most recent update
+        selected_record = self.table.read_record(rid=valid_rid)
+        if selected_record == False:
+            return False
+
+        record_return_list = [] # List of records to be returned
+        for i in range(len(query_columns)):
+            if query_columns[i] == 1:
+                # keep value
+                continue
+            else:
+                # replace value with None
+                selected_record.user_data[i] = None
+
+        record_return_list.append(selected_record)
+        return record_return_list
     """
     # Update a record with specified key and columns
     # Returns True if update is succesful
@@ -51,7 +110,17 @@ class Query:
     """
 
     def update(self, primary_key, *columns):
-        pass
+        columns_list = list(columns)
+        if len(columns_list) != self.table.num_columns:
+            return False
+        if columns_list[0] != None:
+            # You cannot update the primary key
+            return False
+
+        valid_rid = self.table.record_does_exist(primary_key=primary_key)
+        if valid_rid == None:
+            return False
+        
 
     """
     :param start_range: int         # Start of the key range to aggregate 
@@ -63,7 +132,21 @@ class Query:
     """
 
     def sum(self, start_range, end_range, aggregate_column_index):
-        pass
+        summation = 0
+        none_in_range = True
+        for key in range(start_range, end_range + 1):
+            if key not in self.keyToRID:
+                record = False
+            else:
+                baseRID = self.keyToRID[key]
+                selectedPageRange  = self.getPageRange(baseRID)
+                record = self.page_directory[selectedPageRange].select(key, baseRID)
+                none_in_range = False
+                summation += record.columns[aggregate_column_index]
+        if (none_in_range):
+            return False
+        else:
+            return summation
 
     """
     incremenets one column of the record
