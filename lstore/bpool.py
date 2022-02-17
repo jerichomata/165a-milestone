eefc1d5909531d10ef763d2ca24e0f8c1b817bd7
mjpitts <mjpitts@ucdavis.edu>
2022-02-17 12:14:23 -0800

    started bpool.py, still need to finish first 2 functions and create psedo-disk system

---
+++ lstore/bpool.py
@@ -0,0 +1,27 @@
+import table
+
+class bufferpool:
+
+    #intialize a 3x5 bufferpool, this is an arbitrary size. 
+    def __init__(self):
+        MAX_HEIGHT = 3
+        MAX_WIDTH = 5
+        bpool = [[],[],[]]
+
+        #both lists because bufferpool is so small that inefficent searching algorithms don't matter.
+        pinned_pages = []
+        dirty_pages = []
+
+    #mark this page as dirty if it has been updated or recently created and not put to "disk".
+    def mark_dirty(self, page):
+        pass
+
+    #push all updates and creations to "disk".
+    def make_clean(self, dirty_pages):
+        pass
+
+    def pin_page(self, page):
+        self.pinned_pages.append(page)
+
+    def unpin_page(self, page):
+        self.pinned_pages.remove(page)
