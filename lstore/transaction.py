e87b6c17bd5b452270188e948dff51610fe791d0
eykyu <82485287+eykyu@users.noreply.github.com>
2022-03-09 18:45:33 -0800

    Update transaction.py

--- lstore/transaction.py
+++ lstore/transaction.py
@@ -2,7 +2,7 @@ from lstore.table import Table, Record
 from lstore.index import Index
 from lstore.logger import Logger
 import threading
-
+import os
 
 #check_lock -> go to hashtable, 
 #make_shared
@@ -16,6 +16,9 @@ class Transaction:
     """
     def __init__(self):
         self.queries = []
+        self.transaction_ids = []
+        self.transaction_type = []
+        self.tables = []
         self.threading_lock = threading.Lock()
         pass
 
@@ -27,6 +30,7 @@ class Transaction:
     # t.add_query(q.update, grades_table, 0, *[None, 1, None, 2, None])
     """
     def add_query(self, table, query, *args):
+        self.tables.append(table)
         self.queries.append((query, args))
         # use grades_table for aborting
 
@@ -35,22 +39,37 @@ class Transaction:
         for query, args in self.queries:
             #Acquire the lock to check if the record is ok to continue
             self.threading_lock.acquire()
-            can_execute = self.lock_switch(args[0].check_lock)
+            can_execute = self.lock_switch(args[0].check_lock(query.__name__))
             if(can_execute):
                 self.threading_lock.release()
-                result = query(args)
+                if(can_execute == False):
+                    self.abort()
+                else:
+                    #gets transaction_id from log & appends to transactions list
+                    result = query(args)
+                    self.transaction_type.append(query.__name__)
+                    self.transaction_ids.append(result)
             # If the query has failed the transaction should abort
-            if result == False or can_execute == False:
+            if type(result) == bool:
                 return self.abort()
+            else:
+                pass
         return self.commit()
 
     def abort(self):
-        #TODO: do roll-back and any other necessary operations
+        i = 0
+        for table in self.tables:
+            table.undo(self.transaction_type, self.transaction_ids[i])
         return False
 
     def commit(self):
-        # TODO: commit to database
 
+        cwd = os.getcwd()
+        i = 0
+        for j in self.tables:
+            path = cwd + +"\\" + j + "\log\\" + self.transaction_ids[i]
+            os.remove(path)
+            i = i+1
         return True
 
     def lock_switch(self, check_lock, query_type):
